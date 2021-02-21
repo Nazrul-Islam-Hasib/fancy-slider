4305e621252af0fb76f624bd6a7b002c7caa2545
Md. Nazrul Islam <md.-nazrul.islam@stud.uni-bamberg.de>
2021-02-21 13:46:30 +0100

    bug fixed

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const imagesArea = document.querySelector('.images');
 const gallery = document.querySelector('.gallery');
 const galleryHeader = document.querySelector('.gallery-header');
 const searchBtn = document.getElementById('search-btn');
+const searchField = document.getElementById('search');
 const sliderBtn = document.getElementById('create-slider');
 const sliderContainer = document.getElementById('sliders');
+const spinner = document.getElementById('loading-spinner');
 // selected image 
 let sliders = [];
 
@@ -24,31 +26,36 @@ const showImages = (images) => {
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div)
+    toggleSpinner(false);
   })
 
 }
 
 const getImages = (query) => {
-  fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
+  toggleSpinner(true);
+  setTimeout(() => {
+    fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
-    .then(data => showImages(data.hitS))
+    .then(data => showImages(data.hits))
     .catch(err => console.log(err))
+  },500)
 }
 
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
+  element.classList.toggle('added');
  
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
   } else {
-    alert('Hey, Already added !')
+    let imgNo = sliders.indexOf(img);
+    sliders.splice(imgNo, 1);
   }
 }
 var timer
-const createSlider = () => {
+const createSlider = (duration) => {
   // check slider image length
   if (sliders.length < 2) {
     alert('Select at least 2 image.')
@@ -67,7 +74,6 @@ const createSlider = () => {
   document.querySelector('.main').style.display = 'block';
   // hide image aria
   imagesArea.style.display = 'none';
-  const duration = document.getElementById('duration').value || 1000;
   sliders.forEach(slide => {
     let item = document.createElement('div')
     item.className = "slider-item";
@@ -109,14 +115,42 @@ const changeSlide = (index) => {
   items[index].style.display = "block"
 }
 
-searchBtn.addEventListener('click', function () {
-  document.querySelector('.main').style.display = 'none';
-  clearInterval(timer);
-  const search = document.getElementById('search');
-  getImages(search.value)
-  sliders.length = 0;
+// Search image by Keypress
+searchField.addEventListener('keypress', (event) =>{
+  if (event.key == 'Enter') {
+    imageSearch();
+  }
 })
 
-sliderBtn.addEventListener('click', function () {
-  createSlider()
+// Search image by click
+searchBtn.addEventListener('click', function () {
+  imageSearch();
 })
+
+const imageSearch = () =>{
+  document.querySelector('.main').style.display = 'none';
+  clearInterval(timer);
+  getImages(searchField.value)
+  sliders.length = 0;
+}
+
+sliderBtn.addEventListener('click', function () {
+  const duration = document.getElementById('duration').value || 1000;
+  if (duration < 0 ) {
+    alert('Duration can"t be negative')
+  } else {
+    createSlider(duration);
+  }
+})
+
+
+// Spinner
+const toggleSpinner = (show) =>{
+  if (show) {
+    spinner.classList.remove('d-none');
+    imagesArea.classList.add('d-none');
+  } else {
+    spinner.classList.add('d-none');
+    imagesArea.classList.remove('d-none');
+  }
+}
